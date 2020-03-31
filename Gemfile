d3081d2b6fb3e802de3c5bf7a7423f6f0417d290
Robert Clark <rbclark@mitre.org>
2020-03-31 10:55:01 -0400

    Detect lite tags as well (since they are what Github Actions create)

--- Gemfile
+++ Gemfile
@@ -1,7 +1,3 @@
 source "https://rubygems.org"
 
-# Specify your gem dependencies in heimdall_tools.gemspec
-# Since this gem is coming from a Github source it must be here
-gem 'git-version-bump', github: 'rbclark/git-version-bump'
-
 gemspec
--- lib/heimdall_tools/version.rb
+++ lib/heimdall_tools/version.rb
@@ -1,5 +1,8 @@
 require 'git-version-bump'
 
 module HeimdallTools
-  VERSION = GVB.version
+  # Enable lite-tags (2nd parameter to git-version-bump version command)
+  # Lite tags are tags that are used by GitHub releases that do not contain
+  # annotations
+  VERSION = GVB.version(false, true)
 end
